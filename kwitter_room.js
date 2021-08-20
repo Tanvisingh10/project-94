98343488a73c9c04479f585eb7d2dd7c10e61ece
Tanvisingh10 <80450565+Tanvisingh10@users.noreply.github.com>
2021-08-20 20:56:57 +0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDwK6uOYu__5kdBo37DVnLVhS4jsdA1lzk",
+    authDomain: "let-s-chat-web-app-90bcf.firebaseapp.com",
+    databaseURL: "https://let-s-chat-web-app-90bcf-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-web-app-90bcf",
+    storageBucket: "let-s-chat-web-app-90bcf.appspot.com",
+    messagingSenderId: "978177728812",
+    appId: "1:978177728812:web:1956a4f1fa8c71145d0c89"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
